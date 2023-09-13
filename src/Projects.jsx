c73004b54edca61ba2b72b96eb37ab6043a07235
Andreas | YungCat <koalaandi4@gmail.com>
2023-09-14 00:11:56 +0200

    added responsive grid

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -4,24 +4,25 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
 
-  export default function BasicGrid() {
-    return (
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={1}>
-          <Grid xs={4}>
-            <div ></div>
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: 'transparent',
+  borderColor: 'white',
+  borderWidth: 1,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: 'white',
+}));
+
+export default function ResponsiveGrid() {
+  return (
+    <Box sx={{ flexGrow: 1 }} paddingX={{ xs: 4, sm: 16, md: 26}}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {Array.from(Array(6)).map((_, index) => (
+          <Grid xs={2} sm={4} md={4} key={index}>
+            <Item>xs=2</Item>
           </Grid>
-          <Grid xs={4}>
-          </Grid>
-          <Grid xs={4}>
-          </Grid>
-          <Grid xs={4}>
-          </Grid>
-          <Grid xs={4}>
-          </Grid>
-          <Grid xs={4}>
-          </Grid>
-        </Grid>
-      </Box>
-    );
+        ))}
+      </Grid>
+    </Box>
+  );
 }
