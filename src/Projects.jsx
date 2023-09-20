7de6e44adc7a8f1a6cceb37b778658e25a2390ee
Andreas | YungCat <koalaandi4@gmail.com>
2023-09-20 20:49:56 +0200

    styled grid elements + new readme

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,25 +1,36 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
+import Skeleton from '@mui/material/Skeleton';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: 'transparent',
-  borderColor: 'white',
-  borderWidth: 1,
-  padding: theme.spacing(2),
   textAlign: 'center',
   color: 'white',
 }));
 
 export default function ResponsiveGrid() {
   return (
-    <Box sx={{ flexGrow: 1 }} paddingX={{ xs: 4, sm: 16, md: 26}}>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+    <Box sx={{ flexGrow: 1 }} paddingX={{ xs: 8, sm: 14, md: 16, lg: 22, xl: 34 }}>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 0, sm: 3, md: 8, lg: 8, xl: 8 }}
+      >
         {Array.from(Array(6)).map((_, index) => (
-          <Grid xs={2} sm={4} md={4} key={index}>
-            <Item>xs=2</Item>
+          <Grid xs={2} sm={4} md={4} lg={4} xl={2} key={index}  sx={{ maxWidth: '600px' }}>
+            <Item>
+              <Skeleton
+                sx={{
+                  bgcolor: "rgba(255,255,255,.05)",
+                  fontSize: "18rem",
+                  borderRadius: "1.6rem",
+                }}
+                variant="rectangle"
+              />
+            </Item>
           </Grid>
         ))}
       </Grid>
